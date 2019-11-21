a11fa28db6a6ed4f618e9c727e49dda3005e45e3
Tim Garcia <timgarciaa@gmail.com>
2019-11-21 11:19:38 +0800

    Examples for React
    Examples of
    Stateless functional components and Stateful functional components.

---
+++ src/components/Greet.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+// function Greet() {
+//     return <h1>Hello Tim</h1>
+// }
+
+// This is name export, 
+// import { Greet } from './components/Greet';
+// export const Greet = () => <h1>Hello Tim2</h1>
+
+const Greet = () => <h1>Hello Tim2</h1>
+
+export default Greet;
